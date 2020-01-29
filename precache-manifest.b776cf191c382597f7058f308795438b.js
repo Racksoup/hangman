031fdb87264635751cb43459c192c0f157ea5662
Racksoup <39200478+Racksoup@users.noreply.github.com>
2020-01-28 20:42:15 -0500

    Updates

---
+++ precache-manifest.b776cf191c382597f7058f308795438b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f7e89ca5dd99628c0a01147cb75c83",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "2d5771ca4ea3de749e4c",
+    "url": "/hangman/static/css/main.90135742.chunk.css"
+  },
+  {
+    "revision": "777a039be6b5500b4f5a",
+    "url": "/hangman/static/js/2.01581d75.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hangman/static/js/2.01581d75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d5771ca4ea3de749e4c",
+    "url": "/hangman/static/js/main.94af8340.chunk.js"
+  },
+  {
+    "revision": "7c238d90a44709e21cdd",
+    "url": "/hangman/static/js/runtime-main.93f968fc.js"
+  },
+  {
+    "revision": "e2f8a938fb7c986de77b48809f18b6fe",
+    "url": "/hangman/static/media/stage0.e2f8a938.png"
+  },
+  {
+    "revision": "42a9427bd7a5aba65279003fac55d643",
+    "url": "/hangman/static/media/stage1.42a9427b.png"
+  },
+  {
+    "revision": "721c7b8193379631a4b33a97e1b932c7",
+    "url": "/hangman/static/media/stage2.721c7b81.png"
+  },
+  {
+    "revision": "69b5df79dbfa0a09ef804b92fccd86ab",
+    "url": "/hangman/static/media/stage3.69b5df79.png"
+  },
+  {
+    "revision": "dd3821498ab9f30b52d6be409c4d97d6",
+    "url": "/hangman/static/media/stage4.dd382149.png"
+  },
+  {
+    "revision": "f301bf8157b6dbbe70b8bfb96413bef7",
+    "url": "/hangman/static/media/stage5.f301bf81.png"
+  },
+  {
+    "revision": "1784f6701807999a050fb5bbce073dab",
+    "url": "/hangman/static/media/stage6.1784f670.png"
+  }
+]);
